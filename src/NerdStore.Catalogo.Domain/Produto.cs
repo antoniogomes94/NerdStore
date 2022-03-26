4df3598a5a2bf42790e32d6655e8812dedffeff4
Antonio Gomes <antoniogomes313@outlook.com>
2022-03-26 17:59:50 -0300

    Contexto de catalogo e Mapping do Produto e Categoria

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -16,81 +16,65 @@ namespace NerdStore.Catalogo.Domain
         public decimal Valor { get; private set; }
         public DateTime DataCadastro { get; private set; }
         public string Imagem { get; private set; }
-        public decimal QuantidadeEstoque { get; private set; }
-        public Dimensoes Dimensoes { get; protected set; }
+        public int QuantidadeEstoque { get; private set; }
+        public Dimensoes Dimensoes { get; private set; }
         public Categoria Categoria { get; private set; }
 
+        protected Produto() { }
         public Produto(string nome, string descricao, bool ativo, decimal valor, Guid categoriaId, DateTime dataCadastro, string imagem, Dimensoes dimensoes)
         {
-            this.CategoriaId = categoriaId;
-            this.Nome = nome;
-            this.Descricao = descricao;
-            this.Ativo = ativo;
-            this.Valor = valor;
-            this.DataCadastro = dataCadastro;
-            this.Imagem = imagem;
-            this.Dimensoes = dimensoes;
+            CategoriaId = categoriaId;
+            Nome = nome;
+            Descricao = descricao;
+            Ativo = ativo;
+            Valor = valor;
+            DataCadastro = dataCadastro;
+            Imagem = imagem;
+            Dimensoes = dimensoes;
 
             Validar();
         }
 
         public void Ativar() => Ativo = true;
+
         public void Desativar() => Ativo = false;
 
         public void AlterarCategoria(Categoria categoria)
         {
-            this.Categoria = categoria;
-            this.CategoriaId = categoria.Id;
+            Categoria = categoria;
+            CategoriaId = categoria.Id;
         }
 
         public void AlterarDescricao(string descricao)
         {
             Validacoes.ValidarSeVazio(descricao, "O campo Descricao do produto não pode estar vazio");
-            this.Descricao = descricao;
+            Descricao = descricao;
         }
 
         public void DebitarEstoque(int quantidade)
         {
             if (quantidade < 0) quantidade *= -1;
             if (!PossuiEstoque(quantidade)) throw new DomainException("Estoque insuficiente");
-
-            this.QuantidadeEstoque -= quantidade;
+            QuantidadeEstoque -= quantidade;
         }
 
-        public void ReporEstoque(decimal quantidade)
+        public void ReporEstoque(int quantidade)
         {
-            this.QuantidadeEstoque += quantidade;
+            QuantidadeEstoque += quantidade;
         }
 
-        public bool PossuiEstoque(decimal quantidade)
+        public bool PossuiEstoque(int quantidade)
         {
-            return this.QuantidadeEstoque >= quantidade;
+            return QuantidadeEstoque >= quantidade;
         }
 
         public void Validar()
         {
             Validacoes.ValidarSeVazio(Nome, "O campo Nome do produto não pode estar vazio");
             Validacoes.ValidarSeVazio(Descricao, "O campo Descricao do produto não pode estar vazio");
-            Validacoes.ValidarSeDiferente(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
-            Validacoes.ValidarSeMenorQue(Valor, 1, "O campo Valor do produto não pode ser menor igual a 0");
+            Validacoes.ValidarSeIgual(CategoriaId, Guid.Empty, "O campo CategoriaId do produto não pode estar vazio");
+            Validacoes.ValidarSeMenorQue(Valor, 1, "O campo Valor do produto não pode se menor igual a 0");
             Validacoes.ValidarSeVazio(Imagem, "O campo Imagem do produto não pode estar vazio");
-        }
-    }
-
-    public class Categoria : Entity
-    {
-        public string Nome { get; private set; }
-        public int Codigo { get; private set; }
-
-        public Categoria(string nome, int codigo)
-        {
-            this.Nome = nome;
-            this.Codigo = codigo;
-        }
-
-        public override string ToString()
-        {
-            return $"{this.Nome} - {this.Codigo}";
         }
     }
 }
